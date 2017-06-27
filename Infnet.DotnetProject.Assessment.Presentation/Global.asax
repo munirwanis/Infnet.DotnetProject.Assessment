daba95f11795665cd8fc656badca299ebc531dab
Munir Wanis <fallen.angel.pp@gmail.com>
2017-06-26 22:31:28 -0300

    :construction: Ädd MVC Project

---
+++ Infnet.DotnetProject.Assessment.Presentation/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Infnet.DotnetProject.Assessment.Presentation.MvcApplication" Language="C#" %>
---
+++ Infnet.DotnetProject.Assessment.Presentation/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Infnet.DotnetProject.Assessment.Presentation.Startup))]
+namespace Infnet.DotnetProject.Assessment.Presentation
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
